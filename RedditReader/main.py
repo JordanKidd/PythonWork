5b2df12ffa0a63e7c002ea429755f7aa90e2e27e
Jordan <jordan.m.kidd@wmich.edu>
2014-09-08 23:24:43 -0400

    Adding in some error handling

--- RedditReader/main.py
+++ RedditReader/main.py
@@ -3,23 +3,38 @@ __author__ = 'jordankidd'
 from urllib import request
 import json
 
+
 def main():
     again = True
     while again == True:
         subr = input("\nPlease enter the subreddit name to view: ")
         url = "http://www.reddit.com/r/" + str(subr) + "/.json"
-        jsonStr = request.urlopen(url).read().decode("utf-8")
-        jsonObs = json.loads(jsonStr)
-        posts = jsonObs['data']['children']
-        for i in range(1, 6):
-            post = posts[i]
-            title = post['data']['title']
-            print("#" + str(i) + ": " + title)
-        more = input("Look up more? y / n\n")
-        if more == 'y':
-            again = True
-        else:
-            again = False
+        error = False
+
+        try:
+            jsonStr = request.urlopen(url).read().decode("utf-8")
+        except:
+            error = True
+            print("Error occured with data fetch. Is the subreddit spelled correctly?")
+
+        if error == False:
+            jsonObs = json.loads(jsonStr)
+            posts = jsonObs['data']['children']
+            if len(posts) > 5:
+                for i in range(1, 6):
+                    post = posts[i]
+                    title = post['data']['title']
+                    print("#" + str(i) + ": " + title)
+                more = input("Look up more? y / n\n")
+                if more == 'y':
+                    again = True
+                else:
+                    again = False
+            else:
+                print("That subreddit was not found.")
+                again = True
+
 
 if __name__ == '__main__':
     main()
+    print("Goodbye.")
