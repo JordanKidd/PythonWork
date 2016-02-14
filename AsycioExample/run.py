dc386ddb07993aba461b0c25a3994ea718811a27
Jordan <Jordan.M.Kidd@wmich.edu>
2016-02-13 21:10:26 -0600

    small changes

--- AsycioExample/run.py
+++ AsycioExample/run.py
@@ -1,8 +1,10 @@
-# PYTHON 3.5 ASYNCIO EXAMPLE
-import asyncio
+"""
+    PYTHON 3.5 ASYNCIO EXAMPLE
+"""
 import random
 import time
 
+import asyncio
 
 _TASK_NUM = 20
 _WAIT_LOW = 0
@@ -11,20 +13,27 @@ _WAIT_HIGH = 2
 
 # --------------------- ASYNC ------------------------------------
 async def a_compute(x, y):
+    """
+    Adds two numbers using async
+
+    :param x int: The first number
+    :param y int: The second number
+    :return int: Result of adding x and y
+    """
     await asyncio.sleep(random.randint(_WAIT_LOW, _WAIT_HIGH))
     return x + y
 
 
 async def a_print_sum(x, y):
     result = await a_compute(x, y)
-    print("%s + %s = %s" % (x, y, result))
+    print("{} + {} = {}".format(x, y, result))
 
 
 def async():
     loop = asyncio.get_event_loop()
     tasks = []
 
-    for i in range(_TASK_NUM):
+    for _ in range(_TASK_NUM):
         task = a_print_sum(random.randint(1, 10), random.randint(1, 10))
         tasks.append(task)
 
@@ -41,11 +50,11 @@ def compute(x, y):
 
 def print_sum(x, y):
     result = compute(x, y)
-    print("%s + %s = %s" % (x, y, result))
+    print("{} + {} = {}".format(x, y, result))
 
 
 def sync():
-    for i in range(_TASK_NUM):
+    for _ in range(_TASK_NUM):
         print_sum(random.randint(1, 10), random.randint(1, 10))
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
--- NewProcess/main.py
+++ NewProcess/main.py
@@ -1,29 +1,25 @@
-__author__ = 'Jordan'
-
 import subprocess as sp
 from sys import executable
-import child
 from time import sleep
 
-"""
-This is an example of spawning a new python process (with its own interpreter env)
-that handles a new task while the calling one waits.
+__author__ = 'Jordan'
 
-Also, showing a concurrent task as both are running together.
-"""
 
 def main():
+    """
+    This is an example of spawning a new python process (with its own interpreter env)
+    that handles a new task while the calling one waits.
+
+    Also, showing a concurrent task as both are running together.
+    """
+
     print("Parent starting waiting example...")
-    # pid = sp.Popen("python child.py")
-    # pid.wait()
-
-    # OR ONE COULD USE (executable says use THIS python program exec):
-
+    # Executable says use THIS python program exec:
     pid = sp.call([executable, "child.py"])
     print("Parent done waiting.")
 
     print("Parent starting concurrent example...")
-    pid = sp.Popen(["python", "child.py"])
+    pid = sp.Popen([executable, "child.py"])
     for x in range(10):
         print("Parent running:", x)
         sleep(.5)
@@ -31,15 +27,12 @@ def main():
 
     sleep(1)
 
-    """
-    Now an example with using the multiprocessing module
-    """
+    # ------------------  Now an example with using the multiprocessing module:  --------------------
     from multiprocessing import Process
-
 
     print("\nStarting example to pass information and use something in the same file...")
     list_one = ["This", "is", "a", "passed", "list."]
-    p = Process(target=ChildFunction, args=list_one)
+    p = Process(target=childFunction, args=list_one)
     p.start() # runs process
     p.join() # this makes the parent block until the child process terminates
     print("Back in parent process and finished.")
@@ -48,12 +41,12 @@ def main():
 
     print("\nStarting example to pass information and use something in the same file...")
     powers_of_2 = [2**x for x in range(0, 64)]
-    p = Process(target=ChildFunction, args=powers_of_2)
+    p = Process(target=childFunction, args=powers_of_2)
     p.start() # runs process
     print("This will print before the child's list_two.")
 
 
-def ChildFunction(*args):
+def childFunction(*args):
     print("In child and starting to print list...")
     for x in args:
         print(x)
